353e7997ccf04f179452b1a1c83e98632ceba13f
Adriano <Adriano@192.168.25.167>
2016-02-02 18:47:36 -0200

    Institucional UPDATE

--- themes/intranet/pages/institucional.php
+++ themes/intranet/pages/institucional.php
@@ -2,56 +2,45 @@
     <div class="well">
         <article class="row">
             <div class="col-md-6">
-                <img src="https://unsplash.imgix.net/photo-1422222948315-28aadb7a2cb8?w=1024&amp;q=50&amp;fm=jpg&amp;s=cfeadbd7a991e58b553bee29a7eeca55"
+                <h1>Política da Qualidade</h1>
+                <h3>É um resumo das intenções e diretrizes globais do Tommasi relativas à qualidade de seus processos e produtos</h3>
+                <p style="font-size: 1.2em;">“Desafiar-se continuamente na busca de soluções adequadas que atendam às necessidades e expectativas de nossos clientes através de práticas laboratoriais padronizadas e reconhecidas por sua excelência que garantam a obtenção de resultados confiáveis em tempo adequado e suporte técnico-científico aos clientes. O sistema de gestão da qualidade – SGQ, aprovado pela alta direção e que atende os requisitos da Norma ABNT NBR ISO/IEC 17025, Manual para Acreditação de Laboratórios Clínicos da ONA e do DICQ/SBAC, é disseminado entre todos os envolvidos nas operações possibilitando seu aprimoramento constante.”</p>
+            </div>
+            <div class="col-md-6">
+                <img src="<?= HOME ?>/<?= REQUIRE_PATH ?>/images/institucional/politica_da_qualidade.png" width="500px"
+                     class="img-responsive">
+            </div>
+        </article>
+        <article class="row">
+            <div class="col-md-6">
+                <img src="<?= HOME ?>/<?= REQUIRE_PATH ?>/images/institucional/missao.jpg" width="500px"
                      class="img-responsive">
             </div>
             <div class="col-md-6">
                 <h1>Misão</h1>
-                <h3>A subtitle</h3>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-                    ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis
-                    dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies
-                    nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-                    Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In
-                    enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum
-                    felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus
-                    elementum semper nisi.</p>
+                <h3>É o compromisso que o Tommasi assume com seus clientes</h3>
+                <p style="font-size: 1.2em;">“Assegurar, de forma sustentável, soluções laboratoriais seguras, precisas e em tempo hábil”.</p>
             </div>
         </article>
         <article class="row">
             <div class="col-md-6">
                 <h1>Visão</h1>
-                <h3>A subtitle</h3>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-                    ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis
-                    dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies
-                    nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-                    Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In
-                    enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum
-                    felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus
-                    elementum semper nisi.</p>
+                <h3>Onde queremos chegar?</h3>
+                <p style="font-size: 1.2em;">“Estar presente de forma sólida em estados estratégicos brasileiros e em países que apresentem oportunidades de negócios”.</p>
             </div>
             <div class="col-md-6">
-                <img src="https://unsplash.imgix.net/photo-1422222948315-28aadb7a2cb8?w=1024&amp;q=50&amp;fm=jpg&amp;s=cfeadbd7a991e58b553bee29a7eeca55"
+                <img src="<?= HOME ?>/<?= REQUIRE_PATH ?>/images/institucional/visao.png" width="500px"
                      class="img-responsive">
             </div>
         </article>
         <article class="row">
             <div class="col-md-6">
-                <img src="https://unsplash.imgix.net/photo-1422222948315-28aadb7a2cb8?w=1024&amp;q=50&amp;fm=jpg&amp;s=cfeadbd7a991e58b553bee29a7eeca55"
+                <img src="<?= HOME ?>/<?= REQUIRE_PATH ?>/images/institucional/valores.jpg" width="500px"
                      class="img-responsive">
             </div>
             <div class="col-md-6">
                 <h1>Valores</h1>
-                <h3>A subtitle</h3>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-                    ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis
-                    dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies
-                    nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-                    Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In
-                    enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum
-                    felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus
-                    elementum semper nisi.</p>
+                <p style="font-size: 1.2em;">São o coração da cultura organizacional do Tommasi e norteiam as decisões e atitudes da alta direção e demais funcionários.</p>
             </div>
         </article>
     </div>
